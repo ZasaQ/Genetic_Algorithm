99409b065110a597732c9eaf5b532b8d68ed908b
ZasaQ <j.kubowicz7@gmail.com>
2023-06-06 21:07:46 +0200

    adding function checking if polygon is convex

--- PVM/genetic_master.cpp
+++ PVM/genetic_master.cpp
@@ -25,6 +25,30 @@ struct Polygon
     Polygon(const std::vector<Point>& vertices) : vertices(vertices) {}
 };
 
+bool isPolygonConvex(const Polygon& polygon) {
+    const std::vector<Point>& vertices = polygon.vertices;
+    int vertexCount = vertices.size();
+
+    if (vertexCount < 3) {
+        return false;
+    }
+
+    bool isConvex = true;
+    for (int i = 0; i < vertexCount; i++) {
+        const Point& p1 = vertices[i];
+        const Point& p2 = vertices[(i + 1) % vertexCount];
+        const Point& p3 = vertices[(i + 2) % vertexCount];
+
+        float crossProduct = (p2.x - p1.x) * (p3.y - p2.y) - (p2.y - p1.y) * (p3.x - p2.x);
+
+        if (crossProduct < 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 float randFloat(float min, float max)
 {
     std::random_device rd;
@@ -49,9 +73,9 @@ int verticesAmountGeneratedPerPolygon(int min, int max)
     return min + (rand() % (max - min + 1));
 }
 
-void initializePolygons(std::vector<Polygon>& polygons)
+void initializePolygons(std::vector<Polygon>& population)
 {
-    for (auto& InPolygon : polygons)
+    for (auto& inPolygon : population)
     {
         int NumOfGeneratedVertices = verticesAmountGeneratedPerPolygon(3, 12);
 
@@ -59,8 +83,10 @@ void initializePolygons(std::vector<Polygon>& polygons)
         {
             float x = randFloat(0.0f, 10.0f);
             float y = randFloat(0.0f, 10.0f);
-            InPolygon.vertices.push_back(Point(x,y));
+            inPolygon.vertices.push_back(Point(x,y));
         }
+
+
     }
 }
 
